0ac82631e464ecf22d9267e56615a0e3a37624e4
Pavel Sarlov <sarlovpavel@gmail.com>
2022-04-23 15:57:57 +0300

    Add project layout

---
+++ FMI-Materials/src/main/java/com/fmi/materials/FmiMaterialsApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.materials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmiMaterialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmiMaterialsApplication.class, args);
+	}
+
+}
---
+++ FMI-Materials/src/test/java/com/fmi/materials/FmiMaterialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.materials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmiMaterialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
