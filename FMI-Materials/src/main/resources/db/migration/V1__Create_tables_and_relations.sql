afb2efffbc51e7a7686cb61941683e60a9468a1c
Pavel Sarlov <sarlovpavel@gmail.com>
2022-05-05 22:54:00 +0300

    add initial db setup

---
+++ FMI-Materials/src/main/resources/db/migration/V1__Create_tables_and_relations.sql
@@ -0,0 +1,68 @@
+CREATE TABLE Groups(
+    Id SERIAL,
+    Name VARCHAR(10) NOT NULL,
+
+    CONSTRAINT PK_Groups PRIMARY KEY(Id)
+);
+
+CREATE TABLE Courses(
+    Id SERIAL,
+    Name VARCHAR(50) NOT NULL,
+    GroupId INT NOT NULL,
+
+    CONSTRAINT PK_Courses PRIMARY KEY(Id),
+    CONSTRAINT FK_Courses_Groups FOREIGN KEY(Id) REFERENCES Groups(Id) ON DELETE NO ACTION
+);
+
+CREATE TABLE Sections (
+    Id SERIAL,
+    Name VARCHAR(50) NOT NULL,
+    CourseId INT NOT NULL,
+
+    CONSTRAINT PK_Sections PRIMARY KEY(Id),
+    CONSTRAINT FK_Sections_Courses FOREIGN KEY(Id) REFERENCES Courses(Id) ON DELETE NO ACTION
+);
+
+CREATE TABLE FileFormats (
+    Id SERIAL,
+    Name VARCHAR(50) NOT NULL,
+
+    CONSTRAINT PK_FileFormats PRIMARY KEY(Id)
+);
+
+CREATE TABLE Items (
+    Id SERIAL,
+    FileFormat INT NOT NULL,
+    File BYTEA NOT NULL,
+    SectionId INT NOT NULL,
+
+    CONSTRAINT PK_Items PRIMARY KEY(Id),
+    CONSTRAINT FK_Items_Sections FOREIGN KEY(Id) REFERENCES Sections(Id) ON DELETE NO ACTION
+);
+
+CREATE TABLE Users(
+    Id SERIAL,
+    Name VARCHAR(50),
+    Email VARCHAR(50),
+    Password VARCHAR(255),
+
+    CONSTRAINT PK_Users PRIMARY KEY(Id)
+);
+
+CREATE TABLE UserCoursesLists(
+    Id SERIAL,
+    Name VARCHAR(50) NOT NULL,
+    UserId INT NOT NULL,
+
+    CONSTRAINT PK_UserCoursesLists PRIMARY KEY(Id),
+    CONSTRAINT FK_UserCoursesLists_Users FOREIGN KEY(Id) REFERENCES Users(Id) ON DELETE NO ACTION
+);
+
+CREATE TABLE Courses_UserCoursesLists(
+    CourseId INT NOT NULL,
+    UserCoursesListId INT NOT NULL,
+
+    CONSTRAINT PK_Courses_UserCoursesLists PRIMARY KEY(CourseId, UserCoursesListId),
+    CONSTRAINT FK_Courses FOREIGN KEY(CourseId) REFERENCES Courses(Id) ON DELETE NO ACTION,
+    CONSTRAINT FK_UserCoursesLists FOREIGN KEY(UserCoursesListId) REFERENCES UserCoursesLists(Id) ON DELETE NO ACTION
+);
