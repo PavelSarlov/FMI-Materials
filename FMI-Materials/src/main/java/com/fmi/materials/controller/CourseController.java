85bd78bea80117aefa0f0545cc8c98b9fd9f1754
Pavel Sarlov <sarlovpavel@gmail.com>
2022-05-06 10:59:46 +0300

    update controller mappings

--- FMI-Materials/src/main/java/com/fmi/materials/controller/CourseController.java
+++ FMI-Materials/src/main/java/com/fmi/materials/controller/CourseController.java
@@ -1,36 +1,83 @@
 package com.fmi.materials.controller;
 
 import com.fmi.materials.dto.CourseDto;
-import com.fmi.materials.mapper.CourseDtoMapper;
-import com.fmi.materials.model.Course;
 import com.fmi.materials.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/courses")
 public class CourseController {
     @Autowired
     private CourseService courseService;
-    @Autowired
-    private CourseDtoMapper courseDtoMapper;
 
     @PostMapping("courses")
     public ResponseEntity<CourseDto> createCourse(@RequestBody CourseDto courseDto) {
         try {
-            Course course = courseDtoMapper.convertToEntity(courseDto);
-            course = this.courseService.createCourse(course);
-
             return new ResponseEntity<CourseDto>(
-                    courseDtoMapper.convertToDto(course),
+                    this.courseService.createCourse(courseDto),
                     HttpStatus.CREATED
             );
         } catch (Exception e) {
             return new ResponseEntity<CourseDto>(
                     courseDto,
                     HttpStatus.CONFLICT
+            );
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseDto>> findAllCourses() {
+        return new ResponseEntity<List<CourseDto>>(
+                this.courseService.findAllCourses(),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CourseDto> findCourseById(@PathVariable Long id) {
+        try {
+            return new ResponseEntity<CourseDto>(
+                    this.courseService.findById(id),
+                    HttpStatus.FOUND
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<CourseDto>(
+                    HttpStatus.NOT_FOUND
+            );
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity deleteCourseById(@PathVariable Long id) {
+        try {
+            this.courseService.deleteCourse(id);
+
+            return new ResponseEntity(
+                    HttpStatus.OK
+            );
+        } catch (Exception e) {
+            return new ResponseEntity(
+                    HttpStatus.NOT_FOUND
+            );
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<CourseDto> updateCourse(@RequestBody CourseDto courseDto) {
+        try {
+            return new ResponseEntity<CourseDto>(
+                    this.courseService.updateCourse(courseDto),
+                    HttpStatus.FOUND
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<CourseDto>(
+                    courseDto,
+                    HttpStatus.NOT_FOUND
             );
         }
     }
--- FMI-Materials/src/main/java/com/fmi/materials/service/CourseServiceImpl.java
+++ FMI-Materials/src/main/java/com/fmi/materials/service/CourseServiceImpl.java
@@ -1,54 +1,62 @@
 package com.fmi.materials.service;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
+import com.fmi.materials.dto.CourseDto;
+import com.fmi.materials.mapper.CourseDtoMapper;
 import com.fmi.materials.model.Course;
 import com.fmi.materials.repository.CourseRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
 @Service
 public class CourseServiceImpl implements CourseService {
-    private final String ALREADY_EXISTS = "Course with ID = '%s' already exists";
+    private final String ALREADY_EXISTS_MESSAGE = "Course with ID = '%s' already exists";
     private final String NOT_FOUND_MESSAGE = "Course with ID = '%s' not found";
 
     @Autowired
     private CourseRepository courseRepository;
+    @Autowired
+    private CourseDtoMapper courseDtoMapper;
 
     @Override
-    public Course createCourse(Course course) {
-        if(this.courseRepository.existsById(course.getId())) {
-            throw new IllegalArgumentException(String.format(ALREADY_EXISTS, course.getId()));
+    public CourseDto createCourse(CourseDto courseDto) {
+        if(this.courseRepository.existsById(courseDto.getId())) {
+            throw new IllegalArgumentException(String.format(ALREADY_EXISTS_MESSAGE, courseDto.getId()));
         }
-        return this.courseRepository.save(course);
+        Course course = this.courseDtoMapper.convertToEntity(courseDto);
+        return this.courseDtoMapper.convertToDto(this.courseRepository.save(course));
     }
 
     @Override
     public void deleteCourse(Long courseId) {
         if(this.courseRepository.existsById(courseId)) {
-            throw new IllegalArgumentException(String.format(ALREADY_EXISTS, courseId));
+            throw new NoSuchElementException(String.format(NOT_FOUND_MESSAGE, courseId));
         }
         this.courseRepository.deleteById(courseId);
     }
 
     @Override
-    public Course updateCourse(Course course) {
-        if(this.courseRepository.existsById(course.getId())) {
-            throw new IllegalArgumentException(String.format(ALREADY_EXISTS, course.getId()));
+    public CourseDto updateCourse(CourseDto courseDto) {
+        if(this.courseRepository.existsById(courseDto.getId())) {
+            throw new IllegalArgumentException(String.format(ALREADY_EXISTS_MESSAGE, courseDto.getId()));
         }
-        return this.courseRepository.save(course);
+        Course course = this.courseDtoMapper.convertToEntity(courseDto);
+        return this.courseDtoMapper.convertToDto(this.courseRepository.save(course));
     }
 
     @Override
-    public Course findById(Long courseId) {
-        return this.courseRepository.findById(courseId)
-                .orElseThrow(() -> new NoSuchElementException(String.format(NOT_FOUND_MESSAGE, courseId)));
+    public CourseDto findById(Long courseId) {
+        return this.courseDtoMapper.convertToDto(this.courseRepository.findById(courseId)
+                .orElseThrow(() -> new NoSuchElementException(String.format(NOT_FOUND_MESSAGE, courseId))));
     }
 
     @Override
-    public List<Course> findAllCourses() {
-        return this.courseRepository.findAll();
+    public List<CourseDto> findAllCourses() {
+        return this.courseRepository.findAll().stream()
+                .map(this.courseDtoMapper::convertToDto)
+                .collect(Collectors.toList());
     }
 }
