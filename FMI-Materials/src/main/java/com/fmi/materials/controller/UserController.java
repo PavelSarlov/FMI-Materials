91dac181ee03d7009eb31c49daf9f67fd325b539
mddionisie <miro9122@gmail.com>
2022-05-06 22:56:01 +0300

    corrected push to remote branch mistake

--- FMI-Materials/src/main/java/com/fmi/materials/controller/UserController.java
+++ FMI-Materials/src/main/java/com/fmi/materials/controller/UserController.java
@@ -1,2 +1,18 @@
-package com.fmi.materials.controller;public class UserController {
+package com.fmi.materials.controller;
+
+import com.fmi.materials.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
 }
--- FMI-Materials/src/main/java/com/fmi/materials/service/UserServiceImpl.java
+++ FMI-Materials/src/main/java/com/fmi/materials/service/UserServiceImpl.java
@@ -1,2 +1,55 @@
-package com.fmi.materials.service;public class UserServiceImpl {
+package com.fmi.materials.service;
+
+import com.fmi.materials.dto.user.UserDto;
+import com.fmi.materials.dto.user.UserDtoWithId;
+import com.fmi.materials.mapper.UserDtoMapper;
+import com.fmi.materials.model.User;
+import com.fmi.materials.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final String INSTANCE_NOT_FOUND = "User with id: '%s', nof found.";
+    private UserRepository userRepository;
+    private UserDtoMapper userDtoMapper;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, UserDtoMapper userDtoMapper) {
+        this.userRepository = userRepository;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        User user = this.userDtoMapper.convertToEntity(userDto);
+        return this.userDtoMapper.convertToDto(this.userRepository.save(user));
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        if(!this.userRepository.existsById(id)) {
+            throw new NoSuchElementException(String.format(INSTANCE_NOT_FOUND, id));
+        }
+
+        this.userRepository.deleteById(id);
+    }
+
+    @Override
+    public UserDto updateUser(UserDtoWithId userDtoWithId) {
+        if(!this.userRepository.existsById(userDtoWithId.getId())) {
+            throw new NoSuchElementException(String.format(INSTANCE_NOT_FOUND, userDtoWithId.getId()));
+        }
+
+        User user = this.userDtoMapper.convertToEntityWithId(userDtoWithId);
+        return this.userDtoMapper.convertToDto(this.userRepository.save(user));
+    }
+
+    @Override
+    public UserDto findUserById(Long id) {
+        User user = this.userRepository.findById(id).orElseThrow(() -> new NoSuchElementException(String.format(INSTANCE_NOT_FOUND, id)));
+        return this.userDtoMapper.convertToDto(user);
+    }
 }
