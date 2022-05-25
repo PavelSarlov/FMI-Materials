9816286a9c382d58d58c16a2d30fb4808c4e3286
mddionisie <miro9122@gmail.com>
2022-05-25 08:27:51 +0300

    Added create request method

--- backend/src/main/java/com/fmi/materials/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/fmi/materials/service/impl/UserServiceImpl.java
@@ -1,32 +1,64 @@
 package com.fmi.materials.service.impl;
 
+import com.fmi.materials.dto.material.MaterialDto;
+import com.fmi.materials.dto.materialrequest.MaterialRequestDto;
 import com.fmi.materials.dto.user.UserDto;
 import com.fmi.materials.dto.user.UserDtoRegistration;
 import com.fmi.materials.dto.user.UserDtoWithId;
 import com.fmi.materials.exception.EntityAlreadyExistsException;
 import com.fmi.materials.exception.EntityNotFoundException;
 import com.fmi.materials.exception.InvalidArgumentException;
+import com.fmi.materials.mapper.MaterialRequestDtoMapper;
 import com.fmi.materials.mapper.UserDtoMapper;
-import com.fmi.materials.model.User;
+import com.fmi.materials.model.*;
+import com.fmi.materials.repository.MaterialRequestRepository;
+import com.fmi.materials.repository.SectionRepository;
 import com.fmi.materials.repository.UserRepository;
 import com.fmi.materials.service.UserService;
 import com.fmi.materials.vo.ExceptionMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     private UserDtoMapper userDtoMapper;
     private PasswordEncoder passwordEncoder;
+    private SectionRepository sectionRepository;
+    private MaterialRequestRepository materialRequestRepository;
+    private MaterialRequestDtoMapper materialRequestDtoMapper;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, UserDtoMapper userDtoMapper, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository,
+                           UserDtoMapper userDtoMapper,
+                           PasswordEncoder passwordEncoder,
+                           SectionRepository sectionRepository,
+                           MaterialRequestRepository materialRequestRepository,
+                           MaterialRequestDtoMapper materialRequestDtoMapper) {
         this.userRepository = userRepository;
         this.userDtoMapper = userDtoMapper;
         this.passwordEncoder = passwordEncoder;
+        this.sectionRepository = sectionRepository;
+        this.materialRequestRepository = materialRequestRepository;
+        this.materialRequestDtoMapper = materialRequestDtoMapper;
+    }
+
+    private Boolean authenticateCurrentUser(Long userId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetails = (CustomUserDetails)authentication.getPrincipal();
+        Long loggedUserId = userDetails.getId();
+
+        if(loggedUserId != userId) {
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -79,5 +111,21 @@ public class UserServiceImpl implements UserService {
         else {
             throw new EntityNotFoundException(ExceptionMessage.NOT_FOUND.getFormattedMessage("User", "id", userId));
         }
+    }
+
+    @Override
+    public MaterialRequestDto createMaterialRequest(MultipartFile materialFile, Long sectionId, Long userId) throws IOException {
+        Section section = this.sectionRepository.findById(sectionId)
+                .orElseThrow(() -> new EntityNotFoundException(ExceptionMessage.NOT_FOUND.getFormattedMessage("Section", "id", sectionId)));
+
+        if (!authenticateCurrentUser(userId)) {
+            throw new InvalidArgumentException(ExceptionMessage.INVALID_OPERATION.getFormattedMessage());
+        }
+        User user = this.userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(ExceptionMessage.NOT_FOUND.getFormattedMessage("User", "id", userId)));
+
+        MaterialRequest materialRequest = this.materialRequestDtoMapper.convertToEntity(materialFile, user, section);
+        materialRequest = this.materialRequestRepository.save(materialRequest);
+        return this.materialRequestDtoMapper.convertToDto(materialRequest);
     }
 }
