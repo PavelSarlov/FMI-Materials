e8e07fd6daae9e6dc102fa329f830d3fe018a280
mddionisie <miro9122@gmail.com>
2022-06-21 11:38:03 +0300

    refactored backend code

--- backend/src/main/java/com/fmi/materials/repository/CourseListRepository.java
+++ backend/src/main/java/com/fmi/materials/repository/CourseListRepository.java
@@ -1,5 +1,6 @@
 package com.fmi.materials.repository;
 
+import com.fmi.materials.model.Course;
 import com.fmi.materials.model.CourseList;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -16,7 +17,12 @@ public interface CourseListRepository extends CrudRepository<CourseList, Long> {
 
     @Query(value = "SELECT * FROM user_courses_lists cl\n" +
             "WHERE cl.user_id = ?1 AND cl.id = ?2", nativeQuery = true)
-    Optional<CourseList> findUserCourseList(Long userId, Long courseId);
+    Optional<CourseList> findUserCourseList(Long userId, Long courseListId);
+
+    @Query(value = "SELECT c.course_id FROM user_courses_lists cl\n" +
+            "JOIN courses_user_courses_lists c ON c.user_courses_list_id = cl.id\n" +
+            "WHERE cl.user_id = ?1 AND cl.id = ?2 AND c.course_id = ?3", nativeQuery = true)
+    Optional<Integer> findCourseInList(Long userId, Long courseListId, Long courseId);
 
     @Query(value = "SELECT * FROM user_courses_lists cl\n" +
             "WHERE cl.user_id = ?1", nativeQuery = true)
---
+++ backend/src/main/java/com/fmi/materials/repository/FavouriteCoursesRepository.java
@@ -0,0 +1,13 @@
+package com.fmi.materials.repository;
+
+import com.fmi.materials.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface FavouriteCoursesRepository  extends CrudRepository<User, Long> {
+    @Query(value = "SELECT fc.course_id FROM user_favourite_courses fc\n" +
+            "WHERE fc.user_id = ?1 AND fc.course_id = ?2", nativeQuery = true)
+    Optional<Integer> findCourseInFavourites(Long userId,Long courseId);
+}
