269327ee85869ea2c16912b824568dd3b9c7aa01
mddionisie <miro9122@gmail.com>
2022-06-30 22:54:05 +0300

    added tests for adding course to favourite courses

--- backend/src/main/resources/db/migration/h2/V1.2__Add_dummy_data.sql
+++ backend/src/main/resources/db/migration/h2/V1.2__Add_dummy_data.sql
@@ -1,6 +1,10 @@
 INSERT INTO courses(name, description, faculty_department_id, course_group, created_by)
 VALUES ('Web Development with Java', 'WDWJ, Spring Boot, MVC', 13, 1, 'Admin Adminchev');
 
+INSERT INTO courses(name, description, faculty_department_id, course_group, created_by)
+VALUES ('Web Development with C#', '.Net, MVC', 13, 1, 'Admin Adminchev');
+
+
 INSERT INTO sections(name, course_id)
 VALUES ('Home', 1);
 
