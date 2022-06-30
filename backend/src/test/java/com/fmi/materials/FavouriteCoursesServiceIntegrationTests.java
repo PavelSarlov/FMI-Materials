a443e8ddd057904e00c3c08b4217423a5050c8ed
mddionisie <miro9122@gmail.com>
2022-06-30 12:53:26 +0300

    added more test and fixed authentication problems is mocing

--- backend/src/test/java/com/fmi/materials/FavouriteCoursesServiceIntegrationTests.java
+++ backend/src/test/java/com/fmi/materials/FavouriteCoursesServiceIntegrationTests.java
@@ -9,7 +9,6 @@ import com.fmi.materials.service.FavouriteCoursesService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -18,7 +17,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-public class FavouriteCoursesServiceTests {
+public class FavouriteCoursesServiceIntegrationTests {
 
     @Autowired
     private FavouriteCoursesService favouriteCoursesService;
@@ -43,31 +42,5 @@ public class FavouriteCoursesServiceTests {
     public void whenGetFavouriteCoursesWithInvalidUserId_thenThrowEntityNotFoundException() {
         Exception exception = assertThrows(EntityNotFoundException.class, () -> this.favouriteCoursesService.getFavouriteCourses(0L));
         assertThat(exception.getMessage()).isEqualTo("User with id = '0' not found");
-    }
-
-    @Test
-    @Transactional
-    @WithMockUser(username = "u1@email.com", password = "pass", roles = "USER")
-    public void whenDeleteFavouriteCourseWithValidUserIdAndCourseId_thenListSizeIsOneLess() {
-        Integer sizeBeforeDeletion = this.favouriteCoursesService.getFavouriteCourses(1L).size();
-
-        this.favouriteCoursesService.deleteFavouriteCourse(1L, 1L);
-        Integer sizeAfterDeletion = this.favouriteCoursesService.getFavouriteCourses(1L).size();
-
-        assertThat(sizeAfterDeletion+1==sizeBeforeDeletion);
-    }
-
-    @Test
-    @Transactional
-    public void whenDeleteFavouriteCourseWithInvalidUserId_thenThrowEntityNotFoundException() {
-        Exception exception = assertThrows(EntityNotFoundException.class, () ->  this.favouriteCoursesService.deleteFavouriteCourse(0L, 1L));
-        assertThat(exception.getMessage()).isEqualTo("User with id = '0' not found");
-    }
-
-    @Test
-    @Transactional
-    public void whenDeleteFavouriteCourseWithInvalidCourseId_thenThrowEntityNotFoundException() {
-        Exception exception = assertThrows(EntityNotFoundException.class, () ->  this.favouriteCoursesService.deleteFavouriteCourse(1L, 0L));
-        assertThat(exception.getMessage()).isEqualTo("Course with id = '0' not found");
     }
 }
